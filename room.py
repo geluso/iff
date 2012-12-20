f007c8209351d53d38298166d2d75783d7447224
William Chang <mr.williamchang@gmail.com>
2012-12-20 00:03:18 -0800

    Simplifying room title and description properties.

--- room.py
+++ room.py
@@ -1,27 +1,26 @@
 class Room(object):
+
+  title = "No Title"
+  description = "It's dark."
+
   def __init__(self):
     self.exits = {}
-
-  def title(self):
-    return "No Title"
-
-  def description(self):
-    return "It's dark."
 
   def add_exit(self, direction, room):
     self.exits[direction] = room
 
   def __str__(self):
-    return "%s\n%s\n%s\n" % (self.title(), (len(self.title()) * "="), self.description())
+    return "%s\n%s\n%s\n" % (self.title, (len(self.title) * "="), self.description)
 
 class Kitchen(Room):
+
+  title = "The Kitchen"
+
   def __init__(self):
     super(Kitchen, self).__init__() 
     self.light_is_on = False
 
-  def title(self):
-    return "The Kitchen"
-
+  @property
   def description(self):
     return "You are in the kitchen. " + self.light() + " There is a hall south."
 
@@ -32,11 +31,13 @@ class Kitchen(Room):
       return "The lights are off."
 
 class Pantry(Room):
+
+  title = "The Pantry"
+  description = "You stumble through the darkness and trip over a stale loaf of " \
+      "bread. As you fall you grab for something to stable yourself. Your hand falls " \
+      "on the stovetop and is burnt. In a panic you knock over a rack of knives and " \
+      "they soar into the air before plunging into your back. They puncture your " \
+      "lungs. In your last moments, you cut yourself a slice of bread."
+
   def __init__(self):
     super(Pantry, self).__init__() 
-
-  def title(self):
-    return "The Pantry"
-
-  def description(self):
-    return "You stumble through the darkness and trip over a stale loaf of bread. As you fall you grab for something to stable yourself. Your hand falls on the stovetop and is burnt. In a panic you knock over a rack of knives and they soar into the air before plunging into your back. They puncture your lungs. In your last moments, you cut yourself a slice of bread."
